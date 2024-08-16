ad8e781aa66b5e2562eb8aa7925010933207cca8
IT24102890 <it24102890@my.sliit.lk>
2024-08-16 09:33:30 +0530

    Add files via upload

---
+++ Q1/IT24102890Lab4Q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT24102890Lab4Q1 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        // Check if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is Positive.");
+        } else if (number < 0) {
+            System.out.println("The number is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+        
+        scanner.close();
+    }
+}
