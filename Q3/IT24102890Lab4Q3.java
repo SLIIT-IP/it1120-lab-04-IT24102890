c34bd95bb6015c55828377325a0b273fc665b4f1
IT24102890 <it24102890@my.sliit.lk>
2024-08-23 09:16:57 +0530

    Update IT24102890Lab4Q3.java

--- Q3/IT24102890Lab4Q3.java
+++ Q3/IT24102890Lab4Q3.java
@@ -9,15 +9,13 @@ public class IT24102890Lab4Q3 {
         System.out.println("Enter a number: ");
         int number = scanner.nextInt();
         
-        // Check if the number is positive, negative, or zero
-        if (number > 0) {
-            System.out.println("The number is Positive.");
-        } else if (number < 0) {
-            System.out.println("The number is Negative.");
-        } else {
-            System.out.println("The number is Zero.");
-        }
-        
+        // Ternary operator version
+        String Result = (number > 0) ? "The number is Positive."
+                      : (number < 0) ? "The number is Negative."
+                      : "The number is Zero.";
+       
+        System.out.println(result);
+             
         scanner.close();
     }
 }
